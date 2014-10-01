1772c898f1804ea15ad0cc4a5b5259a86469fb3f
ilya-lazarev <ilya.lazarev.i@gmail.com>
2014-10-01 08:56:06 +0400

    Cursor games

--- NexusHVAC/src/NexusHVAC.java
+++ NexusHVAC/src/NexusHVAC.java
@@ -1,3 +1,6 @@
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Cursor;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.*;
 
 public class NexusHVAC {
@@ -10,6 +13,31 @@ public class NexusHVAC {
 		Display display = new Display();
 		Shell shell = new Shell(display);
 		shell.setText("Hello Nexus");
+		
+		
+		shell.setLayout(new FillLayout());
+		Button button = new Button(shell, SWT.PUSH);
+	    button.setText("Change cursor");
+	    
+	    button.addListener(SWT.Selection, new Listener() 
+	    {
+	    	boolean wait=false;
+	    	Cursor cursor;
+	    	
+			@Override
+	        public void handleEvent(Event arg0) 
+	        {
+	            wait = !wait;
+	
+	            if(cursor != null)
+	                cursor.dispose();
+	
+	            cursor = wait ? new Cursor(this. display, SWT.CURSOR_WAIT) : new Cursor(display, SWT.CURSOR_ARROW);
+	
+	            shell.setCursor(cursor);
+	        }
+	    });
+
 		shell.open();
 		while( !shell.isDisposed() )
 		{
